22c0cbff0bd242141d8ebc1a78d8fed60df0f0ae
Sidharth Vijayan <sidharth.vijayan2111@gmail.com>
2026-03-15 17:09:55 +0530

    Include SEO score in popup results
    Added SEO score display to results section.

--- popup.js
+++ popup.js
@@ -3,6 +3,9 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 chrome.tabs.sendMessage(tabs[0].id, {action: "analyze"}, function(response) {
 
 document.getElementById("results").innerHTML = `
+
+<h3>SEO Score: ${response.seoScore}/100</h3>
+
 <p><b>Title Length:</b> ${response.titleLength}</p>
 <p><b>Meta Description:</b> ${response.metaLength}</p>
 <p><b>Word Count:</b> ${response.wordCount}</p>
@@ -10,6 +13,7 @@ document.getElementById("results").innerHTML = `
 <p><b>H2 Count:</b> ${response.h2}</p>
 <p><b>Images:</b> ${response.images}</p>
 <p><b>Images missing ALT:</b> ${response.missingAlt}</p>
+
 `;
 
 });
